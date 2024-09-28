9ff1c0fef614c9071181628fae0c0bda8fdf601f
Ashmi Kartik Parmar <153133539+ashmi2001@users.noreply.github.com>
2024-09-27 20:15:28 -0400

    updated test cases

---
+++ test_triangle.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Updated Jan 21, 2018
+The primary goal of this file is to demonstrate a simple unittest implementation
+
+@author: jrr
+@author: rk
+"""
+
+import unittest
+
+from Triangle import classifyTriangle
+
+# This code implements the unit test functionality
+# https://docs.python.org/3/library/unittest.html has a nice description of the framework
+
+class TestTriangles(unittest.TestCase):
+    # define multiple sets of tests as functions with names that begin
+
+    def testRightTriangleA(self): 
+        self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
+
+    def testRightTriangleB(self): 
+        self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
+        
+    def testEquilateralTriangles(self): 
+        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+
+    def testIsoscelesTriangleA(self): 
+        self.assertEqual(classifyTriangle(2, 2, 3), 'Isosceles', '2,2,3 should be an Isosceles triangle')
+
+    def testIsoscelesTriangleB(self): 
+        self.assertEqual(classifyTriangle(5, 5, 3), 'Isosceles', '5,5,3 should be an Isosceles triangle')
+
+    # Test Scalene Triangle
+    def testScaleneTriangle(self): 
+        self.assertEqual(classifyTriangle(6, 7, 8), 'Scalene', '6,7,8 should be a Scalene triangle')
+
+    # Test for invalid triangles
+    def testNotATriangle(self): 
+        self.assertEqual(classifyTriangle(1, 10, 12), 'NotATriangle', '1,10,12 should not be a valid triangle')
+
+    # Test for invalid inputs (out of range, zeros, negative numbers)
+    def testInvalidInputA(self): 
+        self.assertEqual(classifyTriangle(201, 10, 10), 'InvalidInput', '201,10,10 should be an Invalid Input')
+
+    def testInvalidInputB(self): 
+        self.assertEqual(classifyTriangle(0, 10, 10), 'InvalidInput', '0,10,10 should be an Invalid Input')
+
+    def testInvalidInputC(self): 
+        self.assertEqual(classifyTriangle(-1, 10, 10), 'InvalidInput', '-1,10,10 should be an Invalid Input')
+
+    def testInvalidInputD(self): 
+        self.assertEqual(classifyTriangle(10, "a", 10), 'InvalidInput', '10,"a",10 should be an Invalid Input')
+
+    # Edge case: four inputs
+    def testFourInputs(self):
+        with self.assertRaises(TypeError):  # Expecting TypeError if extra arguments are passed
+            classifyTriangle(1, 1, 1, 1)
+
+    # Edge case: imaginary numbers
+    def testImaginaryNumbers(self):
+        self.assertEqual(classifyTriangle(3+4j, 3+4j, 3+4j), 'InvalidInput', 'Imaginary numbers should be Invalid Input')
+
+    # Edge case: string inputs
+    def testStringInputs(self):
+        self.assertEqual(classifyTriangle('a', 'b', 'c'), 'InvalidInput', 'String inputs should be Invalid Input')
+
+    # Edge case: mixed invalid types
+    def testMixedInputs(self):
+        self.assertEqual(classifyTriangle(10, 20, 'c'), 'InvalidInput', 'Mixed valid and invalid inputs should be Invalid Input')
+
+if __name__ == '__main__':
+    print('Running unit tests')
+    unittest.main()
+
